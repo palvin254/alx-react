3e8a544ec97c0849d6afb0c8e43bf01d5dc540d7
palvin254 <palvinpaul900m@gmail.com>
2023-06-07 23:45:16 -0700

    React_Immutable

--- 0x06-React_Immutable/1-map.js
+++ 0x06-React_Immutable/1-map.js
@@ -3,3 +3,4 @@ import { Map } from 'immutable';
 export default function getImmutableObject(object) {
   return Map(object);
 }
+
--- 0x06-React_Immutable/2-nested.js
+++ 0x06-React_Immutable/2-nested.js
@@ -4,3 +4,4 @@ export default function accessImmutableObject(object, array) {
   const value = fromJS(object);
   return value.getIn(array);
 }
+
